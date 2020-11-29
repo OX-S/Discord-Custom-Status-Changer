0121bd195362feb5660c34ac08b82367dedb925e
Finn <48330226+OX-S@users.noreply.github.com>
2020-11-29 20:11:44 -0300

    Add files via upload

--- CustomStatusBlinker/discordbot.py
+++ CustomStatusBlinker/discordbot.py
@@ -1,69 +1,41 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import requests
+import random
 import time
+import ctypes
+import json
 from configparser import ConfigParser
-from webdriver_manager.chrome import ChromeDriverManager
 
+ctypes.windll.kernel32.SetConsoleTitleW('Discord Custom Status Blinker')
 
 file = 'config.ini'
 config = ConfigParser()
 config.read(file)
+token = config['login']['token']
+messages = ['Probs Doing Fraud','#NeverFold','Big Dick Strength','Dying','Serving the Soviet Union','B E E S E C H U R G E R']
+counter = 0
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+def StatusChanger():
+	global counter
+	if counter < len(messages):
+		counter += 1
+	else:
+		counter = 0
 
+	try:
+		session = requests.Session()
+		headers = {
+			'authorization': token,
+			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.306 Chrome/78.0.3904.130 Electron/7.1.11 Safari/537.36',
+			'content-type': 'application/json'
+		}
+		status = messages[counter]
+		data = '{"custom_status":{"text":"' + status + '"}}'
+		session.patch('https://discordapp.com/api/v6/users/@me/settings', headers=headers, data=data)
 
-driver.get("https://discord.com/login")
-email = driver.find_element_by_xpath("//input[@name=\"email\"]")
-email.send_keys(config['login']['email'])
-password = driver.find_element_by_xpath("//input[@name=\"password\"]")
-password.send_keys(config['login']['password'])
-password.send_keys(Keys.RETURN)
-time.sleep(9)
-n = 5
-while n > 0:
-	driver.find_element_by_xpath("/html/body/div/div[2]/div/div[2]/div/div/div/div[2]/div[1]/section/div/div[1]").click()
+		print('Status Successfully Changed To: ' + status)
+	except:
+		pass
 	time.sleep(1)
-	driver.find_element_by_xpath("/html/body/div/div[6]/div/div/div/div/div[7]").click()
-	time.sleep(1)
-	status = driver.find_element_by_xpath("/html/body/div/div[6]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/input")
-	status.clear()
-	status.send_keys(config['messages']['message1'])
-	status.send_keys(Keys.RETURN)
-	time.sleep(1)
-	driver.find_element_by_xpath("/html/body/div/div[2]/div/div[2]/div/div/div/div[2]/div[1]/section/div/div[1]").click()
-	time.sleep(1)
-	driver.find_element_by_xpath("/html/body/div/div[6]/div/div/div/div/div[7]").click()
-	time.sleep(1)
-	status = driver.find_element_by_xpath("/html/body/div/div[6]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/input")
-	status.clear()
-	status.send_keys(config['messages']['message2'])
-	status.send_keys(Keys.RETURN)
-	time.sleep(1)
-	driver.find_element_by_xpath("/html/body/div/div[2]/div/div[2]/div/div/div/div[2]/div[1]/section/div/div[1]").click()
-	time.sleep(1)
-	driver.find_element_by_xpath("/html/body/div/div[6]/div/div/div/div/div[7]").click()
-	time.sleep(1)
-	status = driver.find_element_by_xpath("/html/body/div/div[6]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/input")
-	status.clear()
-	status.send_keys(config['messages']['message3'])
-	status.send_keys(Keys.RETURN)
-	time.sleep(1)
-	driver.find_element_by_xpath("/html/body/div/div[2]/div/div[2]/div/div/div/div[2]/div[1]/section/div/div[1]").click()
-	time.sleep(1)
-	driver.find_element_by_xpath("/html/body/div/div[6]/div/div/div/div/div[7]").click()
-	time.sleep(1)
-	status = driver.find_element_by_xpath("/html/body/div/div[6]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/input")
-	status.clear()
-	status.send_keys(config['messages']['message4'])
-	status.send_keys(Keys.RETURN)
-	time.sleep(1)
-	driver.find_element_by_xpath("/html/body/div/div[2]/div/div[2]/div/div/div/div[2]/div[1]/section/div/div[1]").click()
-	time.sleep(1)
-	driver.find_element_by_xpath("/html/body/div/div[6]/div/div/div/div/div[7]").click()
-	time.sleep(1)
-	status = driver.find_element_by_xpath("/html/body/div/div[6]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/input")
-	status.clear()
-	status.send_keys(config['messages']['message5'])
-	status.send_keys(Keys.RETURN)
-	time.sleep(1)
-	
+
+while True:
+	StatusChanger()
